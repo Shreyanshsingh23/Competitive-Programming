675283626addf1a519afaa1c40eea3fc424cf686
Shreyansh <shreyanshrewa@gmail.com>
2024-10-27 19:42:06 +0530

    push

---
+++ B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long double
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb emplace_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+   int2(n,l)
+   vi a (n);
+   FOR(i,n) cin >> a[i];
+    sort(all(a));
+   int ans = a[0];
+   FOR(i,n-1){
+    if((a[i+1]-a[i])/2 > ans){
+        ans = (a[i+1]-a[i])/2;
+    }
+   }
+   
+   ans = max(ans,l-a[n-1]);
+   
+   sett(10) << ans << ln;
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
