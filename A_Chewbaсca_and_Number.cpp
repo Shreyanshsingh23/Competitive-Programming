b012454adda84cb40545056c4fb732f99cc03ed5
Shreyansh <shreyanshrewa@gmail.com>
2024-11-01 11:04:22 +0530

    push

---
+++ A_Chewbaсca_and_Number.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb emplace_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+   string s; cin >> s;
+
+   for(int i = 0; i < s.size(); i++){
+        if(i == 0 and s[i] == '9')continue;
+        if(s[i] > '4'){
+            int j = s[i] - '0';
+            j = 9-j;
+            char ch = j + '0';
+            s[i] = ch;
+        }
+   }
+   
+   cout << s << ln;
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ B_Worms.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb emplace_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+   int1(n)
+   vi a (n),pref(n+1,0);
+   FOR(i,n) cin >> a[i];
+   for(int i = 1; i<= n; i++){
+       pref[i] = pref[i-1] + a[i-1];
+       cout << pref[i] << " ";
+   }
+   cout << ln;
+
+
+   int m; cin >> m;
+   while(m--){
+       int1(x)
+       int l = 1, r = n;
+       while(l <= r){
+        int mid = l + (r-l)/2;
+        if(pref[mid] <= x){
+            l = mid+1;
+        }else{
+            r = mid-1;
+        }
+       }
+
+       cout << (l > n? n:l) << ln;
+   }
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
