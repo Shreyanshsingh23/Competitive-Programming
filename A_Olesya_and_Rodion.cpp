3ccfd0b583c7e60445e63f1eae3f0a2e2d4be85a
Shreyansh <shreyanshrewa@gmail.com>
2024-11-02 15:53:12 +0530

    push

---
+++ A_Olesya_and_Rodion.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb emplace_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+
+int n, t; cin >> n >> t;
+
+if(n == 1 and t == 10){
+    cout << -1 << ln;
+    return;
+}   
+
+if(t == 10){
+    FOR(i,n-1) cout << 1 ;
+    cout << 0 << ln;
+    return;
+}
+
+FOR(i,n) cout << t ;
+cout << ln;
+
+
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
