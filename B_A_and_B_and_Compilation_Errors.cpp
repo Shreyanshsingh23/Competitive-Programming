39e4da358a94d0df4b88f730f4ffec735fe14026
Shreyansh <shreyanshrewa@gmail.com>
2024-11-05 23:23:38 +0530

    Add solutions for 7 Codeforces problems and their test cases.

---
+++ B_A_and_B_and_Compilation_Errors.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) (int) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb push_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+   int1(n)
+   int sum1 = 0, sum2 = 0, sum3 = 0,x;
+  
+   FOR(i,n){
+    cin >> x;
+     sum1 += x;
+   }
+   FOR(i,n-1){
+    cin >> x;
+    sum2+= x;
+   }
+   FOR(i,n-2){
+    cin >> x;
+    sum3+= x;
+   }
+   
+
+   cout << sum1 - sum2 << ln;
+   cout << sum2 - sum3 << ln;
+   
+
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ B_Lecture.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) (int) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb push_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+   map<string,string> mp;
+   FOR(i,m){
+    string a,b; cin >> a >> b;
+    if(sz(b) < sz(a)) swap(a,b); //size of a is smaller
+    mp[a] = a;
+    mp[b] = a;
+   }
+//    for(auto [x,y] : mp){
+//     cout << x << ": " << y << ln;
+//    }
+
+   FOR(i,n){
+    string a; cin >> a;
+    cout << mp[a] << ' ';
+   }
+   cout << ln
+   
+}
+
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
