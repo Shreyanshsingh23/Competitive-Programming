d94992435ff989513ee85f44dfe5fa85cf4b0094
Shreyansh <shreyanshrewa@gmail.com>
2024-11-16 17:33:35 +0530

    Update problem C test format and add problem D with solution implementation

---
+++ D_Children_Holiday.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ShreyanshSinghGautam cin.tie(nullptr);cout.tie(nullptr);ios::sync_with_stdio(false);  
+#define int long long
+#define ln '\n';
+#define all(x) x.begin(), x.end()
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define sz(x) (int) x.size()
+#define vi vector<int>
+#define v vector
+#define vii vector<vector<int>>                //vii a (n,vi(m,0))
+#define pii pair<int,int>
+#define int1(t) int t; cin >> t;
+#define int2(n, k) int n, k; cin >> n >> k;
+#define int3(n, k, r) int n, k, r;cin >> n >> k >> r;
+#define pb push_back
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORa(i, a, n) for (int i = a; i < n; i++)
+#define sett(n)          cout<<fixed<<setprecision(n)
+#define F first
+#define S second
+int mex(vi& a, int n){set<int> st {all(a)};int res = 0;while(st.count(res)) res++;return res;}
+typedef pair<int, int> pi;
+
+const bool testcase = 0;
+bool check(vii& a, int m, int n, int x){
+    int cnt = 0;
+    FOR(i,n){
+       
+       
+    }
+    // cout << "cnt: " << cnt << ln;
+    return cnt >= m;
+}
+
+void solve()
+{
+   int m,n; cin >> m >> n;
+   vii a (n,vi(3));
+   for(auto& e : a){
+       for(auto& f : e){
+           cin >> f;
+       }
+   }
+  
+   int l = -1, r = 1e9, mid, ans = 0;
+   while(l <= r){
+    mid = l + (r-l)/2;
+    // cout << "mid: " << mid << ln;
+    if(check(a,m,n,mid)){
+        ans = mid;
+        r = mid-1;
+    }
+    else l = mid+1;
+   }
+// check(a,m,n,1);
+
+   cout << ans << ln;
+   FOR(i,n){
+      int cnt = 0;
+      cnt = (ans/((a[i][0]*a[i][1])+a[i][2]))*a[i][1];
+      if(ans%((a[i][0]*a[i][1])+a[i][2]) >= a[i][0]){
+          cnt += ans%((a[i][0]*a[i][1])+a[i][2])/a[i][0];
+      }
+      cout << cnt << ' ';
+   }
+   cout << ln;
+}
+signed main()
+{
+    ShreyanshSinghGautam
+
+    int t = 1;
+    testcase and cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
