378e41e75a13fb9437bcfe23296207262fc24b71
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2025-01-19 04:32:55 +0530

    Add README.md with repository details

--- A_The_Way_to_Home.cpp
+++ A_The_Way_to_Home.cpp
@@ -53,30 +53,33 @@ int fact [N] ;
 int invFact[N] ;
 void compFact(){fact[0] = 1;for(int i = 1; i < N; ++i)fact[i] = modMul(fact[i-1],i,MOD);invFact[N-1] = modInv(fact[N-1],MOD);for(int i = N-2; i >= 0; --i)invFact[i] = modMul(invFact[i+1],(i+1),MOD);}
 
-const bool testcase = 1;
-v<string> a(2);
-int n;
-bool dfs(int i, int j, set<pi>& vis){
-    if(i == 1 and j == n-1){
-        return true;
+const bool testcase = 0;
+int n,d;
+string s;
+int dp[110];
+int rec(int i, int ans){
+    if(i+d >= n-1){
+        return ans+1;
     }
-    if(i >= 2 or i < 0 or j >= n or j < 0)return false;
-    if(a[i][j] == '1')return false;
-    if(vis.count({i,j}))return false;
-    vis.insert({i,j});
-    return (dfs(i+1,j,vis) or dfs(i-1,j,vis) or dfs(i,j+1,vis) or dfs(i,j-1,vis) or dfs(i+1,j+1,vis) or dfs(i-1,j+1,vis));
+    if(dp[i] != -1)return dp[i];
+    for(int j = d; j > 0; --j){
+        if(s[i+j] == '1'){
+            return dp[i] = rec(i+j,ans+1);
+        }
+
+    }
+    return dp[i] =  MAX;
 }
 void solve()
 {
-   cin >> n;
-    FOR(i,2){
-        cin >> a[i];
-    }
-    // debug(a)
-    set<pi> vis;
-    cout << ( dfs(0,0,vis)? "YES" : "NO") << ln;
-    // debug(vis)
-
+   cin >> n >> d >> s;
+   memset(dp,-1,sizeof(dp));
+   int ans = rec(0,0);
+   if(ans > 100){
+    cout << -1 << ln;
+   }
+   else cout << ans << ln;
+   
 }
 
 signed main()
@@ -88,7 +91,7 @@ signed main()
     // compFact();
     while (t--)
     {
-    //    cout << (solve() ? "YES": "NO") << ln;
+     //   cout << (solve() ? "YES": "NO") << ln;
         solve();
     }
     return 0;
